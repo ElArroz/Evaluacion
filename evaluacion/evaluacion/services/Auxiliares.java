b33e1b8d69e4477070f48e2361932382d9988f50
ElArroz <antonio.chavez@nodalnet.cl>
2020-06-27 12:05:30 -0400

    Trabajo AM...

--- evaluacion/evaluacion/services/Auxiliares.java
+++ evaluacion/evaluacion/services/Auxiliares.java
@@ -3,16 +3,8 @@ package services;
 public class Auxiliares {
 	
 	Ejecutor eje = new Ejecutor();
-	
-	
-	
-	
-	
-	
-	
-	
-	public String iniciarJuego() {
-		//Crear el trablero
+			
+	public String desplegarCarros() {
 		//crear los carros 	3 Kromis(K) - 5 Cahuanos(C) - 10 Trupallas(T)	
 		// llenar los arreglos
 		
