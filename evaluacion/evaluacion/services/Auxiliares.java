3110e511d8bfedff7dded549d51c38c4ca62b87d
ElArroz <antonio.chavez@nodalnet.cl>
2020-06-27 18:09:56 -0400

    Para Victor

--- evaluacion/evaluacion/services/Auxiliares.java
+++ evaluacion/evaluacion/services/Auxiliares.java
@@ -4,7 +4,7 @@ public class Auxiliares {
 	
 	Ejecutor eje = new Ejecutor();
 			
-	public String desplegarCarros() {
+	public String desplegarCarros(int cantidad, String tipo) {  //Usar por Victor
 		//crear los carros 	3 Kromis(K) - 5 Cahuanos(C) - 10 Trupallas(T)	
 		// llenar los arreglos
 		
