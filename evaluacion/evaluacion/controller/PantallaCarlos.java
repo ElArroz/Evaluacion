7552835d0a857fca70e707b9fccf3391f5dee87b
Carlos <Carlos@CHARLYMAN>
2020-06-28 23:26:19 -0400

    carlos

--- evaluacion/evaluacion/controller/PantallaCarlos.java
+++ evaluacion/evaluacion/controller/PantallaCarlos.java
@@ -30,8 +30,15 @@ import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.ListSelectionModel;
 import javax.swing.border.BevelBorder;
+import javax.swing.JTabbedPane;
+import javax.swing.JToolBar;
+import javax.swing.JRadioButton;
+import java.awt.Font;
+import javax.swing.border.SoftBevelBorder;
+import java.awt.FlowLayout;
+import javax.swing.JCheckBox;
 
-public class Pantalla extends JFrame {
+public class PantallaCarlos extends JFrame {
 
 	private JPanel ventana;
 	private JTable tableroJuego;
@@ -41,7 +48,7 @@ public class Pantalla extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Pantalla frame = new Pantalla();
+					PantallaCarlos frame = new PantallaCarlos();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -84,53 +91,22 @@ public class Pantalla extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Pantalla() {
+	public PantallaCarlos() {
 		Ejecutor eje = new Ejecutor();
 		
 		setTitle("[ Batalla por Oscurilandia: A la conquista de la camara secreta ]");
 		setAlwaysOnTop(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(800, 600);
+		setSize(530, 515);
 		setLocationRelativeTo(null);
 		ventana = new JPanel();
 		ventana.setToolTipText("");
-		ventana.setBackground(Color.DARK_GRAY);
+		ventana.setBackground(new Color(51, 102, 0));
 		ventana.setBorder(new EmptyBorder(1, 1, 1, 1));
 		setContentPane(ventana);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setEnabled(false);
-		
-		JButton btnMostrarTablero = new JButton("Mostrar Tablero");
-		btnMostrarTablero.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			mostrarMatriz();
-				}
-		});
-		
-				
-		GroupLayout gl_ventana = new GroupLayout(ventana);
-		gl_ventana.setHorizontalGroup(
-			gl_ventana.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_ventana.createSequentialGroup()
-					.addGroup(gl_ventana.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_ventana.createSequentialGroup()
-							.addGap(98)
-							.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 516, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_ventana.createSequentialGroup()
-							.addGap(309)
-							.addComponent(btnMostrarTablero)))
-					.addContainerGap(168, Short.MAX_VALUE))
-		);
-		gl_ventana.setVerticalGroup(
-			gl_ventana.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_ventana.createSequentialGroup()
-					.addGap(64)
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 265, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(btnMostrarTablero, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
-					.addGap(178))
-		);
+		JScrollPane pantalla = new JScrollPane();
+		pantalla.setEnabled(false);
 		
 		
 		eje.generarMatrix();
@@ -171,9 +147,104 @@ public class Pantalla extends JFrame {
 		
 		}
 		
-		scrollPane.setViewportView(tableroJuego);
+		pantalla.setViewportView(tableroJuego);
+		
+		JPanel menu = new JPanel();
+		menu.setBorder(new SoftBevelBorder(BevelBorder.RAISED, new Color(0, 153, 51), new Color(0, 204, 0), new Color(0, 102, 0), new Color(0, 102, 51)));
+		menu.setBackground(new Color(0, 153, 51));
+		
+		JPanel datos = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) datos.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEADING);
+		flowLayout.setHgap(2);
+		
+		JPanel Logo = new JPanel();
+		GroupLayout gl_ventana = new GroupLayout(ventana);
+		gl_ventana.setHorizontalGroup(
+			gl_ventana.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_ventana.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_ventana.createParallelGroup(Alignment.TRAILING)
+						.addComponent(menu, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 516, Short.MAX_VALUE)
+						.addComponent(pantalla, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 516, Short.MAX_VALUE)
+						.addGroup(gl_ventana.createSequentialGroup()
+							.addComponent(Logo, GroupLayout.PREFERRED_SIZE, 118, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 10, Short.MAX_VALUE)
+							.addComponent(datos, GroupLayout.PREFERRED_SIZE, 364, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
+		);
+		gl_ventana.setVerticalGroup(
+			gl_ventana.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_ventana.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(pantalla, GroupLayout.PREFERRED_SIZE, 265, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_ventana.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(Logo, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(datos, GroupLayout.DEFAULT_SIZE, 87, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(menu, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(48, Short.MAX_VALUE))
+		);
+		
+		JLabel lblNewLabel_2 = new JLabel("** PONER ESTADISTICAS O INFO DEL JUEGO **");
+		datos.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_1 = new JLabel("** PONER IMAGEN **");
+		Logo.add(lblNewLabel_1);
+		menu.setLayout(new GridLayout(2, 0, 5, 5));
+		
+		JButton btnOpcion1CrearJuego = new JButton("Crear Juego");
+		btnOpcion1CrearJuego.setFont(new Font("Trebuchet MS", Font.BOLD, 13));
+		menu.add(btnOpcion1CrearJuego);
+		
+		btnOpcion1CrearJuego.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			// crear juego
+				}
+		});
+		
+		JButton btnOpcion2IniciarPartida = new JButton("Iniciar Partida");
+		btnOpcion2IniciarPartida.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
+		menu.add(btnOpcion2IniciarPartida);
+		
+		btnOpcion2IniciarPartida.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			//iniciar ataque, llamar a metodo	
+			}
+		});
+		
+		JButton btnOpcion3Disparar = new JButton("Disparar");
+		btnOpcion3Disparar.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
+		btnOpcion3Disparar.setBackground(new Color(255, 102, 0));
+		menu.add(btnOpcion3Disparar);
+		
+		btnOpcion3Disparar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			//Disparar	
+			}
+		});
+		
+		JLabel lblNewLabel = new JLabel("MENU");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		menu.add(lblNewLabel);
+		lblNewLabel.setFont(new Font("Stencil", Font.PLAIN, 18));
+		
+		JButton btnOpcion4MostrarTablero = new JButton("Mostrar Tablero");
+		btnOpcion4MostrarTablero.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			mostrarMatriz();	
+			}
+		});
+		
+		btnOpcion4MostrarTablero.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
+		menu.add(btnOpcion4MostrarTablero);
+		
+		JCheckBox chckbxDisparoAleatorio = new JCheckBox("Disparo Aleatorio");
+		chckbxDisparoAleatorio.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
+		chckbxDisparoAleatorio.setSelected(true);
+		menu.add(chckbxDisparoAleatorio);
 		ventana.setLayout(gl_ventana);
 	}
-	
 }
 
