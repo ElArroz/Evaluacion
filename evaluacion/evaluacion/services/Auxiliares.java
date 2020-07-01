409ee9620bcf572c508de7c9e1a84d932ccbe0f6
direyesm <37608963+direyesm@users.noreply.github.com>
2020-06-30 23:40:44 -0400

    Arreglo Victor

--- evaluacion/evaluacion/services/Auxiliares.java
+++ evaluacion/evaluacion/services/Auxiliares.java
@@ -1,69 +1,63 @@
 package services;
 
 import controller.Pantalla;
+import model.Caguano;
+import model.Kromi;
+import model.Trupalla;
 
 public class Auxiliares {
-	
+
 	Ejecutor eje = new Ejecutor();
-			
-	public String desplegarCarros() {  //Usar por Victor
-		//crear los carros 	3 Kromis(K) - 5 Cahuanos(C) - 10 Trupallas(T)	
-		// llenar los arreglos
+
+	public String desplegarCarros() {  
 		
+		int arre[]=eje.generarCoordenadas(null);	//nose si null va aca 
+		int x= arre[0]; 
+		int y= arre[1];
+		int salir = 0;
 		
-		for (int i = 0; i<2; i++)
-		int salir=0;	
-		do
-			int arre[]=eje.generarCoordenadas("K");	
-			int x= arre[0]; 
-			int y= arre[1];
-		
-			
+		//Generar instancia de Kromi
+		for (int i = 0; i<2; i++) {
+		do {
 			if (eje.matrixJuego[x][y]=='·') { 					
-					  if (eje.matrixJuego[x+1][y]=='·'){
-							  if (eje.matrixJuego[x+2][y]=='·'){
-									  eje.setTablero(x, y,'K');
-									  eje.setTablero(x+1,y,'K');
-									  eje.setTablero(x+2,y,'K');
-									  kromi.add(i);
-									  salir=1;
-							  }		  
+				 if (eje.matrixJuego[x+1][y]=='·'){
+					 if (eje.matrixJuego[x+2][y]=='·'){
+						  eje.setTablero(x, y,'K');
+						  eje.setTablero(x+1,y,'K');
+						  eje.setTablero(x+2,y,'K');
+						  Kromi.add(i); // no cacho porque no puedo agregar
+						  salir = 1;
+				 }		  
 			} 
 			}
-			while (salir ==0)
-			//Generar instancia de Kromi
+		}while (salir ==0);
+		}
 		
-		
-			for (int i = 0; i<4; i++)
-			int salir=0;	
-			do
+		//Generar instancia de Caguano
+		for (int i = 0; i<4; i++)	{
+			do{
 				if (eje.matrixJuego[x][y]=='·') { 					
 					  if (eje.matrixJuego[x][y+1]=='·'){
 						  eje.setTablero(x, y,'C');
 						  eje.setTablero(x,y+1,'C');
-						  cagunano.add(i);
-						  salir=1;
+						  Caguano.add(i);
+						  salir = 1;
 						  
-			 while (salir ==0)
-			//Generar instancia de Caguano
-
-	
-			for (int i = 0; i<9; i++)
-			int salir=0;	
-			do
-				if (eje.matrixJuego[x][y]=='·') { 					
-					eje.setTablero(x, y,'K');
-					trupalla.add(i);
-					salir=1;
-			while (salir ==0)	 
-				//Generar instancia de Trupalla
-				
-			return null;
+					  }
+					 }
+				}while (salir == 0);
 		}
-
-*//
-		return null;
+		//Generar instancia de Trupalla
 		
+		for(int i = 0; i < 9; i++)
+			do {
+				if(eje.matrixJuego[x][y] == '.') {
+					eje.setTablero(x, y, 'T');
+					Trupalla.add(i);
+					salir = 1;
+				}
+			}while(salir == 0);
+				
 	}
-	
 }
+	
