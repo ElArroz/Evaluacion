f4025996bd13a18f13bf7edf18ff1e078866f3d5
direyesm <37608963+direyesm@users.noreply.github.com>
2020-07-01 23:21:41 -0400

    Get y Set

--- evaluacion/evaluacion/model/Caguano.java
+++ evaluacion/evaluacion/model/Caguano.java
@@ -2,6 +2,8 @@ package model;
 
 
 public class Caguano extends Carro {
+	
+	//protected static final int ESTADO = 2;
 
 	int alcanceTiro;
 	String colorConfeti;
@@ -12,10 +14,27 @@ public class Caguano extends Carro {
 	}
 
 	public Caguano(int cantidadOcupantes, String fechaIngreso, int estado, int alcanceTiro, String colorConfeti, String p1, String p2) {
-		super(cantidadOcupantes, fechaIngreso, estado);
+		super(cantidadOcupantes, fechaIngreso, 2);
 		this.alcanceTiro = alcanceTiro;
 		this.colorConfeti = colorConfeti;
 		this.p1 = p1;
+		this.p2 = p2;
+	}
+	
+
+	public String getP1() {
+		return p1;
+	}
+
+	public void setP1(String p1) {
+		this.p1 = p1;
+	}
+
+	public String getP2() {
+		return p2;
+	}
+
+	public void setP2(String p2) {
 		this.p2 = p2;
 	}
 
